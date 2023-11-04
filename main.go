ec4e9c169cdf8f81746d198f06d74d2d593b8752
cucuridas@gamil.com <3310223@naver.com>
2023-11-04 23:34:07 +0900

    feature - 예제를 통한 request 객체에서 body 정보 파싱 및 구조체에서 annotation 기능을 통해 json 파싱 돕기

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type User struct {
+	/*
+		Annotation 기능을 통해 json 데이터에서 파싱하고자하는 키 값을 명시 할 수 있음
+		[ex] `json:"first_name"`
+	*/
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 type fooHandler struct {
 }
@@ -22,6 +35,22 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello %s!", name)
 }
 
+func ReadJsonHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request: ", err)
+		return
+	}
+	user.CreatedAt = time.Now()
+
+	data, _ := json.Marshal(user)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
+}
+
 func main() {
 	//Request 요청이 왔을 떄 어떠한 행위를 할 지 handler를 정의하는 함수
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -35,9 +64,10 @@ func main() {
 	mux.HandleFunc("/mux", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hellow Mux")
 	})
-
+	// URL query 요청 처리 API
 	mux.HandleFunc("/bar", barHandler)
-
+	// Body를 통해 json 파일을 처리하는 API
+	mux.HandleFunc("/user", ReadJsonHandler)
 	// 해당 서버의 특정 포트를 열어 request를 대기하도록 하는 함수,
 	// 정의되어진 endpoint와 연관되는 HandleFunc을 실행시키도록 하는 함수
 	// handler에 mux를 객체를 넣지 않을 경우 mux 핸들러를 제외한 모든 핸들러에 대한 요청을 처리
