5b1f0f0728ff21cdff0431a992d348713064ba99
cucuridas@gamil.com <3310223@naver.com>
2023-11-06 01:56:41 +0900

    repactoring - 기존 main 함수에서 mux의 하위 핸들러를 정의하는 로직을 NewMux 함수에서 처리하여 main에서 사요하도록 수정

--- main.go
+++ main.go
@@ -6,19 +6,8 @@ import (
 )
 
 func main() {
-	// 새로운 mux 객체 생성
-	rt := router.NewMux()
-	// example headler 추가 - handler 구조체를 전달 받는 방법
-	rt.Handle("/foo", &router.FooHandler{})
-	// example handler 추가 - handler 함수를 전달받아 처리하는 방법
-	rt.HandleFunc("/bar", router.BarHandler)
-	// user handler 추가
-	rt.HandleFunc("/user", router.ReadJsonHandler)
-
-	rt.HandleFunc("/uploads", router.FileHandler)
-	rt.Handle("/", http.FileServer(http.Dir("./public")))
 	// 해당 서버의 특정 포트를 열어 request를 대기하도록 하는 함수,
 	// 정의되어진 endpoint와 연관되는 HandleFunc을 실행시키도록 하는 함수
 	// handler에 mux를 객체를 넣지 않을 경우 mux 핸들러를 제외한 모든 핸들러에 대한 요청을 처리
-	http.ListenAndServe(":3000", rt)
+	http.ListenAndServe(":3000", router.NewMux())
 }
--- myapp/mux.go
+++ myapp/mux.go
@@ -4,5 +4,17 @@ import "net/http"
 
 func NewMux() *http.ServeMux {
 	mux := http.NewServeMux()
+
+	// handler 추가 및 정의
+	// example headler 추가 - handler 구조체를 전달 받는 방법
+	mux.Handle("/foo", &FooHandler{})
+	// example handler 추가 - handler 함수를 전달받아 처리하는 방법
+	mux.HandleFunc("/bar", BarHandler)
+	// user handler 추가
+	mux.HandleFunc("/user", ReadJsonHandler)
+	// file upload handler 추가
+	mux.HandleFunc("/uploads", FileHandler)
+	mux.Handle("/", http.FileServer(http.Dir("./public")))
+
 	return mux
 }
