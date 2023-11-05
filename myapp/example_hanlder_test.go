306e9a89be590cc79081743580f2c4beb23d58cc
cucuridas@gamil.com <3310223@naver.com>
2023-11-05 16:06:49 +0900

    testing - user 기능을 테스트하기 위한 테스크 코드 작성

--- myapp/example_hanlder_test.go
+++ myapp/example_hanlder_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 // 테스트 코드 내에서 사용하게될 공용 변수인 mux를 전역변수 처리
-var mux = NewMux()
+var exam_mux = NewMux()
 
 // 'example_handler.go' 패키지에서 정의한 FooHandler 객체를 테스트하는 함수
 func TestExampleFooHandelr(t *testing.T) {
@@ -17,8 +17,8 @@ func TestExampleFooHandelr(t *testing.T) {
 	res := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "/foo", nil)
 
-	mux.Handle("/foo", &FooHandler{})
-	mux.ServeHTTP(res, req)
+	exam_mux.Handle("/foo", &FooHandler{})
+	exam_mux.ServeHTTP(res, req)
 
 	data, _ := ioutil.ReadAll(res.Body)
 	test.Equal("Hello foo", string(data))
@@ -36,8 +36,8 @@ func TestExampleBarHanlder_WithOutName(t *testing.T) {
 	req := httptest.NewRequest("GET", "/bar", nil)
 
 	//전역 변수 mux에 handler 함수 초기화
-	mux.HandleFunc("/bar", BarHandler)
-	mux.ServeHTTP(res, req)
+	exam_mux.HandleFunc("/bar", BarHandler)
+	exam_mux.ServeHTTP(res, req)
 
 	// assertration 객체의 Equal 함수를 통해 원하는 값이 전달되었는지 확인
 	test.Equal(http.StatusOK, res.Code)
@@ -57,7 +57,7 @@ func TestExampleBarHanlder_WithName(t *testing.T) {
 	res := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", "/bar?name=CHOI", nil)
 
-	mux.ServeHTTP(res, req)
+	exam_mux.ServeHTTP(res, req)
 
 	data, _ := ioutil.ReadAll(res.Body)
 	test.Equal("Hello CHOI!", string(data))
--- myapp/user_handler_test.go
+++ myapp/user_handler_test.go
@@ -1,7 +1,43 @@
 package myapp
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var user_mux = NewMux()
 
 func TestExamHandler(t *testing.T) {
+	test := assert.New(t)
+	userData := &User{
+		FirstName: "CHOi",
+		LastName:  "EUN",
+		Email:     "3310223@naver.com",
+	}
+	jsonUserData, err := json.Marshal(userData)
 
+	if err != nil {
+		t.Fatal("Error in work to convert json data struct")
+	}
+
+	res := httptest.NewRecorder()
+	/*
+		json 타입으로 encoding 된 데이터를 request 객체에 담기위해서는 Buffer 형태의 데이터로
+		변환이 필요함 따라서 bytes.NewBuffer() 함수를 통해 디코딩하는 과정이 필요로 함
+	*/
+
+	req := httptest.NewRequest("GET", "/user", bytes.NewBuffer(jsonUserData))
+
+	user_mux.HandleFunc("/user", ReadJsonHandler)
+	user_mux.ServeHTTP(res, req)
+
+	test.Equal(http.StatusOK, res.Code)
+
+	/*
+		비교하고자하는 구조체와
+	*/
 }
