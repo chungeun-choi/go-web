851977610e09b890be016e9f97f47296578c2540
cucuridas@gamil.com <3310223@naver.com>
2023-11-06 17:46:13 +0900

    repact - UserHandler 테스트를 위한 코드를 mock 서버를 생성하여 테스트하도록 리팩토링

--- myapp/user_handler_test.go
+++ myapp/user_handler_test.go
@@ -11,14 +11,17 @@ import (
 
 func TestExamHadler_WithOutJson(t *testing.T) {
 	test := assert.New(t)
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/user", nil)
+	// mock server를 통해 테스트를 진행할 수 있도록 변형
+	mockServer := httptest.NewServer(NewMux())
+	defer mockServer.Close()
 
-	user_mux := http.NewServeMux()
-	user_mux.HandleFunc("/user", ReadJsonHandler)
-	user_mux.ServeHTTP(res, req)
+	resp, err := http.Get(mockServer.URL + "/user")
 
-	test.Equal(http.StatusBadRequest, res.Code)
+	if err != nil {
+		t.Fatal(http.StatusInternalServerError)
+		return
+	}
+	test.Equal(http.StatusBadRequest, resp.StatusCode)
 }
 
 func TestExamHandler_WithJson(t *testing.T) {
@@ -49,4 +52,35 @@ func TestExamHandler_WithJson(t *testing.T) {
 	err := json.NewDecoder(res.Body).Decode(user)
 	test.Nil(err)
 	test.Equal("choi", user.FirstName)
+}
+
+func TestUserWithJson(t *testing.T) {
+	/*
+		mock server를 통해 테스트를 진행하기
+		테스트 구조는 'TestExamHandler_WithJson' 함수 동작과 동일하나
+		mock 서버를 생성해서 테스트를 진행하는 구조임에 따라 req,res 객체 없이 method 함수만을
+		호출하여 테스트 진행
+	*/
+	test := assert.New(t)
+	requestData := `{"first_name":"choi","last_name":"eun","email":"3310223@naver.com"}`
+	mockServer := httptest.NewServer(NewMux())
+	defer mockServer.Close()
+
+	resp, err := http.Post(mockServer.URL+"/user", "application/json", strings.NewReader(requestData))
+
+	if err != nil {
+		t.Fatal(http.StatusBadRequest)
+		return
+	}
+	// status code 200인지 확인
+	test.Equal(http.StatusOK, resp.StatusCode)
+	// 전달받은 결과의 값이 같은 지 field 값을 비교하기
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+	if err != nil {
+		t.Fatal("Faile to decoding json string")
+	}
+	test.Equal("choi", user.FirstName)
+	test.Equal("eun", user.LastName)
+
 }
